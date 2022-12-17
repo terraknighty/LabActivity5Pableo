afc68d53b40b45213fd04293e301d596235789d7
Terraknight <johnjosephpableo@gmail.com>
2022-12-17 14:31:40 +0800

    Complete FoodOrdering

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -3,10 +3,14 @@ package FoodOrdering;
 
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FoodOrderGUI extends JFrame {
     private JPanel panel;
-    private JCheckBox pizzaCheckBox;
+    private JCheckBox cPizza;
     private JRadioButton rbNone;
     private JButton btnOrder;
     private JCheckBox cBurger;
@@ -18,6 +22,29 @@ public class FoodOrderGUI extends JFrame {
     private JRadioButton rb10;
     private JRadioButton rb15;
 
+    private List <JCheckBox> coFoods;
+    private List <JRadioButton> coDiscount;
+
+    public FoodOrderGUI(){
+        coFoods = new ArrayList<>();
+        coFoods.add(cPizza);
+        coFoods.add(cBurger);
+        coFoods.add(cFries);
+        coFoods.add(cSoftDrinks);
+        coFoods.add(cTea);
+        coFoods.add(cSundae);
+        coDiscount = new ArrayList<>();
+        coDiscount.add(rbNone);
+        coDiscount.add(rb5);
+        coDiscount.add(rb10);
+        coDiscount.add(rb15);
+        btnOrder.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                computerOrder();
+            }
+        });
+    }
     public static void main(String[] args) {
         FoodOrderGUI app = new FoodOrderGUI();
         app.setContentPane(app.panel);
@@ -25,6 +52,42 @@ public class FoodOrderGUI extends JFrame {
         app.setDefaultCloseOperation(EXIT_ON_CLOSE);
         app.setVisible(true);
         app.setTitle("Food Ordering System");
+    }
+
+    public void computerOrder(){
+        double total = 0;
+        for(JCheckBox cb: coFoods){
+            if(cb.isSelected()){
+                if(cb == cPizza){
+                    total += 100;
+                }else if(cb == cBurger){
+                    total += 80;
+                }else if(cb == cFries){
+                    total += 65;
+                }else if(cb == cSoftDrinks){
+                    total += 55;
+                }else if(cb == cTea){
+                    total += 50;
+                }else if(cb == cSundae){
+                    total += 40;
+                }
+            }
+        }
+        for(JRadioButton cb: coDiscount){
+            if(cb.isSelected()){
+                if(cb == rbNone){
+                    //None
+                }else if(cb == rb5){
+                    total -= (total * .05);
+                }else if(cb == rb10){
+                    total -= (total * .1);
+                }else if(cb == rb15){
+                    total -= (total *.15);
+                }
+            }
+        }
+
+        JOptionPane.showMessageDialog(panel, String.format("The total price is Php %.2f", total));
     }
 
 }
