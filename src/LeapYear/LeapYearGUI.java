c621264bd9c1ae4b26ff66efed7041b5abe09b37
Terraknight <johnjosephpableo@gmail.com>
2022-12-17 10:30:52 +0800

    Add exception handling to LeapYear

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -14,31 +14,31 @@ public class LeapYearGUI extends JFrame{
 
     public LeapYearGUI(){
 //        try{
-            btnCheckYear.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
+        btnCheckYear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                checkLeapYear();
+            }
+        });
+
+        tfYear.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if(e.getKeyCode() == KeyEvent.VK_ENTER){
                     checkLeapYear();
                 }
-            });
+            }
 
-            tfYear.addKeyListener(new KeyListener() {
-                @Override
-                public void keyTyped(KeyEvent e) {
+            @Override
+            public void keyReleased(KeyEvent e) {
 
-                }
-
-                @Override
-                public void keyPressed(KeyEvent e) {
-                    if(e.getKeyCode() == KeyEvent.VK_ENTER){
-                        checkLeapYear();
-                    }
-                }
-
-                @Override
-                public void keyReleased(KeyEvent e) {
-
-                }
-            });
+            }
+        });
 
 
     }
@@ -53,22 +53,31 @@ public class LeapYearGUI extends JFrame{
     }
 
     public void checkLeapYear(){
-        int year = Integer.parseInt(tfYear.getText());
-        if (year % 4 == 0) {
-            if (year % 100 == 0) {
-                if (year % 400 == 0) {
-                    JOptionPane.showMessageDialog(panel, "Leap Year");
-                }else {
-                    JOptionPane.showMessageDialog(panel, "Not a leap year");
-                }
-            }else {
-                JOptionPane.showMessageDialog(panel, "Leap Year");
+        try{
+            int year = Integer.parseInt(tfYear.getText());
+            if(year < 0 ){
+                throw new InputMismatchException("Input must not be a negative number");
+            }else if (year == 0){
+                throw new InputMismatchException("Input must be a nonzero");
             }
-        }else {
-            JOptionPane.showMessageDialog(panel, "Not a leap year");
+            if (year % 4 == 0) {
+                if (year % 100 == 0) {
+                    if (year % 400 == 0) {
+                        JOptionPane.showMessageDialog(panel, "Leap Year");
+                    } else {
+                        JOptionPane.showMessageDialog(panel, "Not a leap year");
+                    }
+                } else {
+                    JOptionPane.showMessageDialog(panel, "Leap Year");
+                }
+            } else {
+                JOptionPane.showMessageDialog(panel, "Not a leap year");
+            }
+        }catch (InputMismatchException e){
+            JOptionPane.showMessageDialog(panel, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+        }catch(NumberFormatException e){
+            JOptionPane.showMessageDialog(panel, "Input must be an integer", "Error", JOptionPane.ERROR_MESSAGE);
         }
-
     }
 
 }
-
