ea39bcc18043d2ffcc818011ef77ea1cbaa98c73
Terraknight <johnjosephpableo@gmail.com>
2022-12-17 09:28:15 +0800

    Rename LeapYear components

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -1,9 +1,16 @@
 package LeapYear;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class LeapYearGUI {
-    private JPanel panel1;
-    private JTextField textField1;
-    private JButton checkButton;
+public class LeapYearGUI{
+    private JPanel panel;
+    private JTextField tfYear;
+    private JButton btnCheckYear;
+
+
+
 }
