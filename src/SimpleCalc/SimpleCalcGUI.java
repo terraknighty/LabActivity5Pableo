efbd7e0ee07c9159ce07c72a944603b0e6822452
Terraknight <johnjosephpableo@gmail.com>
2022-12-17 12:38:17 +0800

    Complete SimpleCalc

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -3,6 +3,10 @@ package SimpleCalc;
 import LeapYear.LeapYearGUI;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SimpleCalcGUI extends JFrame{
     private JPanel panel;
@@ -12,6 +16,18 @@ public class SimpleCalcGUI extends JFrame{
     private JTextField tfNumber2;
     private JTextField lblResult;
 
+
+    public SimpleCalcGUI(){
+
+        btnCompute.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                calculate();
+            }
+        });
+
+    }
+
     public static void main(String[] args) {
         SimpleCalcGUI app = new SimpleCalcGUI();
         app.setContentPane(app.panel);
@@ -19,5 +35,27 @@ public class SimpleCalcGUI extends JFrame{
         app.setDefaultCloseOperation(EXIT_ON_CLOSE);
         app.setVisible(true);
         app.setTitle("Simple Calculator");
+    }
+
+    public void calculate(){
+        int firstNum = Integer.parseInt(tfNumber1.getText());
+        int secondNum = Integer.parseInt(tfNumber2.getText());
+        int operation = cbOperations.getSelectedIndex();
+        int result = 0;
+        switch (operation) {
+            case 0:
+                result = firstNum + secondNum;
+                break;
+            case 1:
+                result = firstNum - secondNum;
+                break;
+            case 2:
+                result = firstNum * secondNum;
+                break;
+            case 3:
+                result = firstNum / secondNum;
+                break;
+        }
+        lblResult.setText(Integer.toString(result));
     }
 }
